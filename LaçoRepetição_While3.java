a25d5f3fb5938407a9e6e4d47b17f8ee0ee30eab
Juliagali <juliagalibarroso@gmail.com>
2022-09-09 14:27:24 -0300

    solicitar idades até que seja inserido -99

---
+++ LaçoRepetição_While3.java
@@ -0,0 +1,34 @@
+package turma58;
+import java.util.Scanner;
+public class LaçoRepetição_While3 {
+	
+	public static void main(String[] args) {
+	Scanner leia = new Scanner(System.in);
+	
+	/* 3- Solicitar a idade de várias pessoas e imprimir: Total de pessoas com menos de
+21 anos. Total de pessoas com mais de 50 anos. O programa termina quando
+idade for =-99. (WHILE)*/
+	
+		int idade=0, menos21=0, mais50=0;
+		
+		System.out.println("Insira sua idade: ");
+		idade = leia.nextInt();
+		
+		while (idade != -99) {
+			System.out.println("Insira sua idade: ");
+			idade = leia.nextInt();
+			
+			if (idade<21) {
+				menos21++;
+			}if (idade>50) {
+				mais50++;
+			}		
+		}
+		
+		System.out.println("\nA quantidade de pessoas com menos de 21 anos é de: " + menos21);
+		System.out.println("A quantidade de pessoas com mais de 50 anos é de: " + mais50);
+		
+		
+	}
+
+}
